c442fcff83606c5ff73e3af9af3417ca8c6fde1f
Lzx0122 <102167717+Lzx0122@users.noreply.github.com>
2022-04-11 16:16:21 +0800

    2022-04-11

--- 52屆winform/UserControl_shipping_manage .Designer.cs
+++ 52屆winform/UserControl_shipping_manage .Designer.cs
@@ -36,6 +36,16 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -102,10 +112,97 @@
             this.textBox4.Size = new System.Drawing.Size(100, 22);
             this.textBox4.TabIndex = 7;
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(733, 30);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "船種";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(768, 27);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(40, 84);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 12);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "船公司";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(81, 81);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(199, 84);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 12);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "船東";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(258, 81);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 14;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 139);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(893, 324);
+            this.dataGridView1.TabIndex = 15;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(467, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "新增";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(557, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "刪除";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // UserControl1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox3);
@@ -117,6 +214,7 @@
             this.Name = "UserControl1";
             this.Size = new System.Drawing.Size(899, 466);
             this.Load += new System.EventHandler(this.UserControl1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -132,5 +230,14 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- 52屆winform/UserControl_shipping_manage .cs
+++ 52屆winform/UserControl_shipping_manage .cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace _52屆winform
@@ -14,15 +15,113 @@ namespace _52屆winform
     {
 
         private C_SQLServerLib lib;
+        C_SQLconn conclass = new C_SQLconn();
+        SqlDataReader connReader;
+        SqlCommand cmd;
         public UserControl1(C_SQLServerLib e)
         {
             InitializeComponent();
             lib = e;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            reloading();
         }
 
         private void UserControl1_Load(object sender, EventArgs e)
         {
 
+        }
+
+
+        private void reloading()
+        {
+
+            dataGridView1.DataSource = lib.GetDataSet($"select IMO,英文船名,中文船名,載噸重,船舶種類中文名稱,船商名稱,船東 from 船舶基本資料" +
+                $" inner join 船種 on 船舶基本資料.船種DI=船種.船種ID" +
+                $" inner join 船公司 on 船舶基本資料.船公司ID=船公司.船公司ID ", "船舶基本資料").Tables["船舶基本資料"];
+
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridView1.SelectedRows[0].Index != -1)
+                {
+                    if (dataGridView1.SelectedRows[0].Cells[0].Value != null) { 
+                        textBox1.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+                        textBox2.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+                        textBox3.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+                        textBox4.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+                        textBox5.Text = dataGridView1.SelectedRows[0].Cells[4].Value.ToString();
+                        textBox6.Text = dataGridView1.SelectedRows[0].Cells[5].Value.ToString();
+                        textBox7.Text = dataGridView1.SelectedRows[0].Cells[6].Value.ToString();
+
+
+
+
+
+
+                    }
+                }
+
+            }
+            catch (Exception ex) { 
+            
+            
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            string isNull = lib.GetSQLvalue($"select IMO from 船舶基本資料 where IMO='{textBox1.Text}'", "IMO");
+            try
+            {
+                if (isNull == null)
+                {
+                    cmd = new SqlCommand();
+                    conclass.openConn();
+                    cmd.CommandText = $"insert into 船舶基本資料 (IMO,英文船名,中文船名,載噸重,船種DI,船公司ID,船東) " +
+                        $"select '{textBox1.Text}','{textBox2.Text}','{textBox3.Text}','{textBox4.Text}'" +
+                        $",(select 船種ID from 船種 where 船舶種類中文名稱='{textBox5.Text}')," +
+                        $"(select 船公司ID from 船公司 where 船商名稱='{textBox6.Text}'),'{textBox7.Text}'";
+                    cmd.Connection = conclass.conn;
+                    cmd.ExecuteNonQuery();
+                    reloading();
+                    MessageBox.Show("修改完成", "訊息");
+                }
+                else {
+
+                    MessageBox.Show("IMO資料不可重複", "錯誤");
+                }
+            }
+            catch (Exception ex) {
+
+               
+
+            }
+           
+            
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cmd = new SqlCommand();
+                conclass.openConn();
+                cmd.CommandText = $"DELETE FROM 船舶基本資料 WHERE IMO='{textBox1.Text}' ";
+                cmd.Connection = conclass.conn;
+                cmd.ExecuteNonQuery();
+                reloading();
+                MessageBox.Show($"已成功刪除{textBox1.Text}", "訊息");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("","錯誤");
+            }
         }
     }
 }
